f40c895f0783fd10d563f81da639df1349a4ce56
MuhammadHamza5 <mh03142456338@gmail.com>
2025-02-25 14:57:45 +0500

    creaate campaign done

--- organization.sol
+++ organization.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
-contract organization{
-    struct Campaign{
-        uint owner;
+
+contract Organization {
+    struct Campaign {
+        address owner;
         string name;
         string description;
         uint256 fund_required;
@@ -11,31 +12,39 @@ contract organization{
         string image;
         uint256 deadLine;
     }
-    mapping(uint256 => Campaign) public  Campaigns;
-    uint256 public  numOfListCampaign = 0;
 
+    mapping(uint256 => Campaign) public Campaigns;
+    uint256 public numOfListCampaign = 0;
+
+    event campaignCreateSuccess  (string _message , address _owner );
+
+    // Function to create a new campaign
     function createCampaign(
-        uint _owner,
+        address _owner,
         string memory _name,
         string memory _description,
         uint256 _fund_required,
         uint256 _recive_amount,
         string memory _image,
         uint256 _deadLine
-    ) public  returns (bool){
-        Campaign newCampaign = Campaigns[numOfListCampaign];
-        
-        newCampaign.owner = _owner;
-        newCampaign.name=_name;
-        newCampaign.description = _description;
-        newCampaign.fund_required =  _fund_required;
-        newCampaign.recive_amount  = _recive_amount;
-        newCampaign.image = _image;
-        newCampaign.deadLine=_deadLine;
-        Campaigns[numOfListCampaign] =newCampaign;
+    ) public returns (bool) {
+
+        require(_fund_required > 0,"Fund required must be greater than 0");
+        require(_deadLine > block.timestamp,"Deadline must be after current time");
+
+        Campaign memory newCampaign = Campaign({
+            owner: _owner,
+            name: _name,
+            description: _description,
+            fund_required: _fund_required,
+            recive_amount: _recive_amount,
+            image: _image,
+            deadLine: _deadLine
+        });
+
+        Campaigns[numOfListCampaign] = newCampaign;
         numOfListCampaign++;
-    }
-
-
+        emit campaignCreateSuccess("Campaign Create Successfully" , _owner);
+        return true; // Returning true upon successful campaign creation
     }
 }
